8741a5a5bb3c29a47623dcfb3137ea50e553a2e6
MrAnde7son <mizrahi.itamar@gmail.com>
2026-04-15 18:08:01 +0300

    feat: add favicon to the application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastProvider, ErrorBoundary } from '@hakunahq/ui'
+import faviconUrl from '@hakunahq/ui/assets/favicon.svg'
 import App from './App.jsx'
 import '@hakunahq/ui/tokens/colors.css'
 import '@hakunahq/ui/tokens/base.css'
 import './index.css'
+
+const faviconLink = document.querySelector("link[rel~='icon']") ?? document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'icon' }))
+faviconLink.type = 'image/svg+xml'
+faviconLink.href = faviconUrl
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
